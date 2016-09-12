e4c1749d8e31216ba77f04e6ec629b3c83e027ad
Galina Avdeeva <cornhedgehog@gmail.com>
2016-09-12 12:18:31 +0200

    Update helpers.go

--- helpers.go
+++ helpers.go
@@ -10,7 +10,10 @@ type WeatherValue struct {
 
 //cloudiness -	облачность по градациям: 0 - ясно, 1- малооблачно, 2 - облачно, 3 - пасмурно
 //precipitation -	тип осадков: 4 - дождь, 5 - ливень, 6,7 – снег, 8 - гроза, 9 - нет данных, 10 - без осадков
-var WeatherValues = []WeatherValue{WeatherValue{"ясно", "0"}, WeatherValue{"малооблачно", "1"}, WeatherValue{"облачно", "2"}, WeatherValue{"пасмурно", "3"}, WeatherValue{"дождь", "4"}, WeatherValue{"ливень", "5"}, WeatherValue{"небольшой снег", "6"}, WeatherValue{"сильный снег", "7"}, WeatherValue{"гроза", "8"}, WeatherValue{"нет данных", "9"}, WeatherValue{"без осадков", "10"}}
+var WeatherValues = []WeatherValue{WeatherValue{"ясно", "0"}, WeatherValue{"малооблачно", "1"}, 
+WeatherValue{"облачно", "2"}, WeatherValue{"пасмурно", "3"}, WeatherValue{"дождь", "4"}, WeatherValue{"ливень", "5"}, 
+WeatherValue{"небольшой снег", "6"}, WeatherValue{"сильный снег", "7"}, WeatherValue{"гроза", "8"}, 
+WeatherValue{"нет данных", "9"}, WeatherValue{"без осадков", "10"}}
 
 func GetPresipitation(cloudiness string, presipitation string) string {
 	cloudiness_int, _ := strconv.Atoi(cloudiness)
